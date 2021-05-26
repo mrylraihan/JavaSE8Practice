ab4d7eb01abab2c3846b38e60c6f973675b6cb8c
mrylraihan <mr.ylraihan@gmail.com>
2021-05-26 13:59:45 -0400

    More Predicate Practice

---
+++ src/com/intertech/lambdaPractice4/PredicateDemo3.java
@@ -0,0 +1,42 @@
+package com.intertech.lambdaPractice4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class PredicateDemo3 {
+
+	
+	public static void main(String[] args) {
+		Predicate<String> predicate3 = (String t) ->{
+			if(t == "Wallie") {
+				return true;
+			}else {
+				return false;
+			}
+		
+		};
+		
+	boolean test1 = predicate3.test("Wallie");
+	boolean test2 = predicate3.test("Amy");
+	System.out.println(test1);
+	System.out.println(test2);
+	
+	List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
+	
+	Predicate<Integer> predicate = (x)-> x > 5;
+	
+	Predicate<Integer> predicate1 = (x)-> x < 11;
+	
+	list.stream().filter(predicate.and(predicate1)).collect(Collectors.toList()).forEach(System.out::println);
+	
+	}
+	
+	
+	
+
+}
+	
+
