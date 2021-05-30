5b037d2bb2689414dd2c512d47bcabf1e1aafaee
mrylraihan <mr.ylraihan@gmail.com>
2021-05-30 13:50:15 -0400

    more static imports and lambdas

--- src/com/intertech/staticImportsPractice/Test3.java
+++ src/com/intertech/staticImportsPractice/Test3.java
@@ -2,6 +2,9 @@ package com.intertech.staticImportsPractice;
 import static com.intertech.staticImportsPractice.SafeDriver.*;
 public class Test3 {
 	public static void main(String[] args) {
+		
+		//Static imports allowing you to import static methods 
+		//and variables from an interface
 		int howFast = MAXIUM_SPEED;
 		int follingOtherCars = SAFE_FOLLOWING_DISTANCE;
 		double fastestSpeed = MAXIUM_SPEED;
@@ -9,6 +12,10 @@ public class Test3 {
 		System.out.println(howFast);
 		System.out.println(follingOtherCars);
 		System.out.println(fastestSpeed);
+		//Practicing lambda functions
+		StartVehicle lambdatest = ()->System.out.println("Starting Car");
+		
+		lambdatest.startCar();
 		
 	}
 
