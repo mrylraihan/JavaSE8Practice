fa9c06a86d5c5da6a873df531da8ec9d84aa545b
mrylraihan <mr.ylraihan@gmail.com>
2021-05-31 15:17:39 -0400

    Local Date and Time Practice

---
+++ src/DateAndTimePractice/DateTimeExamples.java
@@ -0,0 +1,15 @@
+package DateAndTimePractice;
+
+import java.time.LocalDate;
+
+public class DateTimeExamples {
+	public static void p(Object o) {
+		System.out.println(o);
+	}
+
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+		
+		System.out.println(now);
+	}
+}
