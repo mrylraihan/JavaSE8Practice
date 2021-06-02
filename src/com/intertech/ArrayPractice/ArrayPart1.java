0f64da4232a4c4a6ae5bca23961636d096f25904
mrylraihan <mr.ylraihan@gmail.com>
2021-06-02 15:56:05 -0400

    Java 8 arrays practice

---
+++ src/com/intertech/ArrayPractice/ArrayPart1.java
@@ -0,0 +1,54 @@
+package com.intertech.ArrayPractice;
+
+public class ArrayPart1 {
+	public static void main(String[] args) {
+
+		// both declarations are valid
+//	int intArray[];
+		// or
+//	int [] intArray2;
+//	
+//	//multiple arrays in one line
+//	char[] charArray1, charArray2;
+
+		// how to instantiate an array
+
+		int[] intArray = new int[10];
+		intArray[0] = 1;
+		intArray[1] = 2;
+		intArray[2] = 3;
+		intArray[3] = 4;
+		intArray[4] = 5;
+		intArray[5] = 6;
+		intArray[6] = 7;
+		intArray[7] = 8;
+		intArray[8] = 9;
+		intArray[9] = 10;
+		// how to declare , initialize, and as sign an array
+		int[] intArray2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+		int[] intArray3 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+		int[] data;
+		data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+		int[] intArray4;
+		intArray4 = new int[] { 1, 2, 3, 4 };
+
+		System.out.println(intArray.length);
+		System.out.println(intArray2.length);
+		System.out.println(intArray3.length);
+		System.out.println(intArray4.length);
+
+		int[] anArray = new int[10];
+		for (int i = 0; i < anArray.length; i++) {
+			anArray[i] = i * i;
+			System.out.println(anArray[i]);
+		}
+		System.out.println("--------------------------->");
+		for (int i = 0; i < intArray2.length; i++) {
+			System.out.println("intArray2 :" + intArray2[i]);
+		}
+
+	}
+}
---
+++ src/com/intertech/ArrayPractice/CopyArrayMeth.java
@@ -0,0 +1,41 @@
+package com.intertech.ArrayPractice;
+
+import java.util.Iterator;
+
+import com.acme.utils.MyDate;
+
+public class CopyArrayMeth {
+	public static void main(String[] args) {
+		int[] sourceArray = { 1, 2, 3, 4, 5, 6 };
+		int[] destinationArray = new int[4];
+		System.arraycopy(sourceArray, 0, destinationArray, 0, 3);
+
+		for (int i = 0; i < destinationArray.length; i++) {
+			System.out.println(destinationArray[i]);
+		}
+
+		int[] array = { 1, 2, 3 };
+		int[] temp = array;
+		array = new int[4];
+		System.arraycopy(temp, 0, array, 0, temp.length);
+		temp = null;
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
+		MyDate[] dates = new MyDate[3];
+		dates[0] = new MyDate(10, 26, 1999);
+		dates[1] = new MyDate(10, 01, 1991);
+		dates[2] = new MyDate(2, 16, 1992);
+
+		for (int i = 0; i < dates.length; i++) {
+			System.out.println(dates[i]);
+		}
+		
+		MyDate[] moreDates = { new MyDate(10, 26, 1999),  new MyDate(10, 26, 1999),  new MyDate(10, 26, 1999)};
+		
+		String [] names = {"Wallie", "Mike", "Ryan"};
+	}
+
+}
