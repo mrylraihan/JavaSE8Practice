4e9b74a3a21e1119a02138bba6da231415f66438
Wayel Raihan <mr.ylraihan@gmail.com>
2021-06-08 12:18:20 -0400

    Iterator and ListIteratorPractice

--- src/com/intertech/ArrayListPractice1/EqualsPointer2.java
+++ src/com/intertech/ArrayListPractice1/EqualsPointer2.java
@@ -1,14 +1,44 @@
 package com.intertech.ArrayListPractice1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class EqualsPointer2 {
 	public static void main(String[] args) {
 		ArrayList<String> birds = new ArrayList<String>();
 		birds.add("hawk");
 		birds.add(1, "robin");
-		birds.add(0,"blue jay");
+		birds.add(0, "blue jay");
 		birds.add(1, "cardinal");
 		System.out.println(birds);
+
+		for (String bird : birds) {
+			System.out.println(bird);
+		}
+		
+		Iterator iter = birds.iterator();
+		while(iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+		System.out.println("--------------------> Iterator");
+		ArrayList languages = new ArrayList();
+		languages.add("Java");
+		languages.add("JavaScript");
+		languages.add("Html");
+		
+		Iterator iL = languages.iterator();
+		while(iL.hasNext()) {
+			System.out.println(iL.next());
+		}
+		
+		System.out.println("--------------------> Iterator");
+		
+		ListIterator iL2 = languages.listIterator();
+		System.out.println(iL2.hasNext());
+		System.out.println(iL2.nextIndex());
+
 	}
 }
