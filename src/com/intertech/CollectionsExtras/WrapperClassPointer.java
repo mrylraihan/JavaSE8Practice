3b2f5a5958859bf601e338fe04a5c85d7ad4d6d8
mrylraihan <mr.ylraihan@gmail.com>
2021-06-11 10:20:14 -0400

    more stuff on Wrapperclasses

--- src/com/intertech/CollectionsExtras/WrapperClassPointer.java
+++ src/com/intertech/CollectionsExtras/WrapperClassPointer.java
@@ -3,6 +3,9 @@ package com.intertech.CollectionsExtras;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.intertech.ArrayPractice2.Employee;
+import com.intertech.ArrayPractice2.Secretary;
+
 public class WrapperClassPointer {
 	public static void main(String[] args) {
 		System.out.println(Integer.sum(3, 4));
@@ -11,6 +14,7 @@ public class WrapperClassPointer {
 		List list = new ArrayList();
 		int j = 7;
 		int y = 8;
+		double d = 3.00;
 		list.add(Integer.valueOf(j));
 		list.add(y);
 		System.out.println(list);
@@ -21,5 +25,24 @@ public class WrapperClassPointer {
 		Character wrapChar = Character.valueOf('r');
 		char r = wrapChar.charValue();
 		
+		Double wrapDouble = Double.valueOf(d);
+		double dd = wrapDouble.doubleValue();
+		
+		//etc.. with all the wrapper classes 
+		
+		List staff = new ArrayList();
+		Employee e = new Employee();
+		e.name = "Wallie";
+		staff.add(e);
+		Secretary s = new Secretary();
+		s.name = "Shaj";
+		staff.add(s);
+		String x = "Mr.Raihan";
+		staff.add(x);
+		System.out.println(staff);
+		Object bill = staff.get(0);
+		Object bestSec = (Secretary)staff.get(1);
+		System.out.println(bill);
+		System.out.println(bestSec);
 	}
 }
