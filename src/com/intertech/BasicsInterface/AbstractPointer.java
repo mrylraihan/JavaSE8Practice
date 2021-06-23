6ed1215edf18f57a47520df0438abbb0599f7201
Wayel Raihan <mr.ylraihan@gmail.com>
2021-06-23 19:15:48 -0400

    more default interfaces methods

--- src/com/intertech/BasicsInterface/AbstractPointer.java
+++ src/com/intertech/BasicsInterface/AbstractPointer.java
@@ -1,5 +1,5 @@
 package com.intertech.BasicsInterface;
 
 public abstract class AbstractPointer implements Interface2{
-
+public abstract void astractMInAbstrctClass();
 }
--- src/com/intertech/BasicsInterface/ConcertClass.java
+++ src/com/intertech/BasicsInterface/ConcertClass.java
@@ -4,13 +4,23 @@ public class ConcertClass extends AbstractPointer{
 
 	@Override
 	public void abstractMethod() {
-		// TODO Auto-generated method stub
+		System.out.println("Abstract method that needs to be overridden from our interface1");
+		
+	}
+	@Override
+	public void astractMInAbstrctClass() {
+	System.out.println("abstract method in our abstract class that need to be overridden");
 		
 	}
 	public static void main(String[] args) {
-		Interface1 class1 = new ConcertClass();
+		ConcertClass class1 = new ConcertClass();
 		class1.defaultMethod();
+		class1.abstractMethod();
+		class1.astractMInAbstrctClass();
 		Interface1.staticMethod();
+		Interface1 class2 = new ConcertClass();
+		class2.defaultMethod();
 	}
+	
 
 }
