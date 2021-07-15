0c724d1ac402f11f2f25d889c6229d0feb501cce
mrylraihan <mr.ylraihan@gmail.com>
2021-07-15 08:42:52 -0400

    swapping practice solved revised

--- src/com/newStuff/Practice/Question2.java
+++ src/com/newStuff/Practice/Question2.java
@@ -1,40 +1,36 @@
 package com.newStuff.Practice;
 
 public class Question2 {
-	
+
 	public static void main(String[] args) {
-		//1234567;2
-		String numbs = "12345678910";
-		int n = 3;
+		// 1234567;2
+		String numbs = "123456789";
+		int n = 2;
 		int cur = 0;
 		char[] chaArray = numbs.toCharArray();
-		String newNumList =new String("");
-		for (int i = 0; i < chaArray.length; i+=n) {
-			cur+=n;
+		String newNumList = "";
+		for (int i = 0; i < chaArray.length; i += n) {
+			cur += n;
 			int start = i;
-			int end = cur-1;
-			if (end > chaArray.length-1) {
+			int end = cur - 1;
+			if (end > chaArray.length - 1) {
 				break;
 			}
 			char tmp = chaArray[start];
 			chaArray[start] = chaArray[end];
 			chaArray[end] = tmp;
-			
-			newNumList = new String(chaArray);
+
+//			newNumList = new String(chaArray);
 		}
-		
-		System.out.println(newNumList);
-//		System.out.println(numbs.length());
-		
-		
-//		for (char singleN : chaArray) {
-//		   if (singleN == chaArray[chaArray.length-1]) {
-//			System.out.println(singleN);
-//		}else {
-//			System.out.print(singleN+",");
-//		}
-//			
-//		}
+
+		for (char singleN : chaArray) {
+			if (singleN == chaArray[chaArray.length - 1]) {
+				newNumList = newNumList + singleN;
+			} else {
+				newNumList = newNumList + singleN + ",";
+			}
+		}
+		System.out.println(newNumList + "; "+n);
 	}
 
 }
