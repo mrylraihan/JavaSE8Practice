af88ee3d12a5b0cece15e574fa5dbcdcd557c1e0
mrylraihan <mr.ylraihan@gmail.com>
2021-07-30 11:05:48 -0400

    twoSum

--- src/com/newStuff/Practice/TestQuestion.java
+++ src/com/newStuff/Practice/TestQuestion.java
@@ -1,6 +1,27 @@
 package com.newStuff.Practice;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestQuestion {
-//test
-	//another test
+	
+	public boolean twoSum(int[] nums, int target) {
+		Set<Integer>set = new HashSet<>();
+		for(int num :nums) {
+			if(set.contains(num)) {
+				return true;
+			}
+			set.add(target - num);
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int [] numberList = {4,7,5,8};
+		int target = 10;
+		
+		boolean answer = new TestQuestion().twoSum(numberList, target);
+		System.out.println(answer);
+		
+	}
 }
